a0301be0c0a20f0f5a3f3e9a4f98cebe84624478
polskapeeps <poitrkostka@gmail.com>
2025-05-06 18:23:58 -0400

    Add Firebase initialization and configuration to script.js

--- src/Renderer/script.js
+++ src/Renderer/script.js
@@ -1,3 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDqusLzhEc3GvFElGRdi6mjxtxhLuInYrA",
+  authDomain: "grind-time-747f4.firebaseapp.com",
+  projectId: "grind-time-747f4",
+  storageBucket: "grind-time-747f4.firebasestorage.app",
+  messagingSenderId: "406101223329",
+  appId: "1:406101223329:web:bca312115c3b61181dcde0",
+  measurementId: "G-1ZE97D6KCC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 document.addEventListener("DOMContentLoaded", () => {
   // ---=== DOM Elements ===---
   const hourlyRateInput = document.getElementById("hourlyRate");
