5147032179455a32fa3e565238c99dc5e2ebfc66
polskapeeps <poitrkostka@gmail.com>
2025-05-19 23:10:29 -0400

    Add gh-pages dependency and update Vite configuration for repository deployment

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/GrindTime-WrkHrs/', // <-- IMPORTANT: Replace '/Hours-Tracker/' with '/Your-Repository-Name/'
+});
