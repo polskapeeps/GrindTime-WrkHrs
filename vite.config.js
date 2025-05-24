e4d5607cb1f086c4ddb45e54dca704faab3657be
polskapeeps <poitrkostka@gmail.com>
2025-05-24 15:10:52 -0400

    Removes unnecessary comment from build config
    Cleans up configuration by deleting a redundant comment,
    improving readability without altering functionality.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '/GrindTime-WrkHrs/',
   build: {
-    assetsDir: '_app_assets', // <--- Key change
+    assetsDir: '_app_assets',
     rollupOptions: {
       input: {
         main: './index.html',
